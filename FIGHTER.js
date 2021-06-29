7803080ab5fdea62747f1bf23ca9a2569e14bf0d
unknown <alicecemails@gmail.com>
2021-06-29 15:29:23 +0100

    Added planning for JS.

---
+++ FIGHTER.js
@@ -0,0 +1,3 @@
+//JS will include:
+//-A class that will be used for the two fighters. This consists of a HP value, Energy value, and three functions for altering these values (HP loss, Energy gain, Energy loss from Super). 
+//Event listeners for all of P1's buttons, for performing each attack and determining the random attack P2 will use. This will change image indexing and call the fighter class functions based on if statements. P2's buttons are purely cosmetic and should only change to highlight when their Supers are available.
